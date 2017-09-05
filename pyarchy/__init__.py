0de84848fdd2f0212d473100952de6fbf777943e
Zach Gates <thezachgates@gmail.com>
2017-09-05 18:38:51 -0500

    PyPi compatible

--- pyarchy/__init__.py
+++ pyarchy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.5'
+__version__ = '1.1.8'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (C) 2017 Zach Gates'
 
@@ -15,5 +15,16 @@ __all__ = [
     'utils',
 ]
 
-for module in __all__:
-    __import__('pyarchy.%s' % module)
+try:
+    import os
+    import sys
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    for module in __all__:
+        __import__('pyarchy.' + module)
+
+    del os
+    del sys
+except NameError:
+    pass
